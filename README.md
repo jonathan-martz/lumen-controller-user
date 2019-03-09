b111c073698ef0434c7e6650b9dc00de71c393e7
jonathan <bitbucket@jmartz.de>
2019-03-09 11:34:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Installations
+
+add to routes/web.php
+```php
+$router->get('/user/view', 'UserController@view');
+```
---
+++ controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \http\Env\Response;
+use \Illuminate\Http\Request;
+use \Illuminate\Support\Facades\DB;
+use \Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * @param  Request  $request
+     * @return Response
+     */
+    public function view(Request $request){
+        $validation = $this->validate($request, [
+
+        ]);
+
+        $todo = DB::connection('mysql.read')
+            ->table('users')
+            ->where('id','=',$request->user()->getAuthIdentifier());
+
+        $count = $todo->count();
+
+        if($count === 1){
+            $this->addResult('user',$todo->first());
+            $this->addMessage('success','Your User Data.');
+        }
+        else{
+            $this->addMessage('success','User doesnt exists.');
+        }
+
+        return $this->getResponse();
+    }
+}
