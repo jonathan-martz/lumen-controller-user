f237ddd3262171e7af105f23cb448ee9d756f9fb
jonathan-martz <github@jmartz.de>
2019-12-31 17:15:36 +0100

    [TEST-123] replace todo with users

--- controller/UserController.php
+++ controller/UserController.php
@@ -2,33 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use \http\Env\Response;
-use \Illuminate\Http\Request;
-use \Illuminate\Support\Facades\DB;
-use \Illuminate\Support\Facades\Hash;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     /**
-     * @param  Request  $request
+     * @param Request $request
      * @return Response
      */
-    public function view(Request $request){
+    public function view(Request $request)
+    {
         $validation = $this->validate($request, [
 
         ]);
 
-        $todo = DB::table('users')
-            ->where('id','=',$request->user()->getAuthIdentifier());
+        $users = DB::table('users')
+            ->where('id', '=', $request->user()->getAuthIdentifier());
 
-        $count = $todo->count();
+        $count = $users->count();
 
-        if($count === 1){
-            $this->addResult('user',$todo->first());
-            $this->addMessage('success','Your User Data.');
+        if($count === 1) {
+            $this->addResult('user', $users->first());
+            $this->addMessage('success', 'Your User Data.');
         }
-        else{
-            $this->addMessage('success','User doesnt exists.');
+        else {
+            $this->addMessage('success', 'User doesnt exists.');
         }
 
         return $this->getResponse();
